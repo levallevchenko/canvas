723753994c1f150d141cbb25791c9bd1516eb2ac
Anna Levchenko <anna.levchenko@quadcode.com>
2024-07-26 12:24:35 +0300

    add second animation

--- canvas.js
+++ canvas.js
@@ -30,7 +30,7 @@ class Particle {
     this.y = Math.random() * canvas.height;
     // this.x = mouse.x;
     // this.y = mouse.y;
-    this.size = Math.random() * 5 + 1;
+    this.size = Math.random() * 25 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
   }
@@ -38,18 +38,20 @@ class Particle {
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
+
+    if (this.size > 0.2) this.size -= 0.1;
   }
 
   draw() {
     ctx.fillStyle = "blue";
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
   }
 }
 
 const init = () => {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     particles.push(new Particle());
   }
 };
@@ -59,6 +61,11 @@ const handleParticles = () => {
   for (let i = 0; i < particles.length; i++) {
     particles[i].update();
     particles[i].draw();
+
+    if (particles[i].size <= 0.3) {
+      particles.splice(i, 1);
+      i--;
+    }
   }
 };
 
